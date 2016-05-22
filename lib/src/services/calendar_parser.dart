958dcf43e4ee7042a76463bbc01420e5cbc7c642
Joel Trottier-Hebert <iamquebecois@gmail.com>
2016-05-22 16:19:27 -0400

    WIP

--- lib/src/services/calendar_parser.dart
+++ lib/src/services/calendar_parser.dart
@@ -1,9 +1,10 @@
 import '../ical.dart';
 
 class CalendarParser {
-  void parse(String input) {
+  CalendarObject parse(String input) {
     final tokens = tokenizeCalendar(input);
     final calendar = parseCalendarObjects(tokens);
     var bob = 1;
+    return calendar;
   }
 }
